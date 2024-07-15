14ce56846f237962721c343a6954072d3ab82606
Oguzhan Yilmaz <oguzhan@hepapi.com>
2024-07-15 19:10:01 +0300

    update example 1

--- examples/01_filter_tags_basic.py
+++ examples/01_filter_tags_basic.py
@@ -15,12 +15,12 @@ data_list = [
 
 print("#"*20, f"data: {dumps(data_list,indent=2)}", "", "#"*20, "", sep="\n")
 
-expression = '[] | filter_tags(`["Name="]`, @)' # it's important that your expression array must be inside `` backticks
+expression = '[] | filter_tags(`["Name="]`, @)[].Tags' # it's important that your expression array must be inside `` backticks
 search_name_tag=jmespath.search(expression, data_list, options=jmespath_options)
 print("--*--"*10, f"Using jmespath expression: '{expression}'", dumps(search_name_tag, indent=2), "", sep='\n')
 
 
  
-expression = '[].filter_tags(`["*=1234"]`, @)' # it's important that your expression array must be inside `` backticks
+expression = '[].filter_tags(`["*=1234"]`, @)[].Tags' # it's important that your expression array must be inside `` backticks
 search_tag_value_1234=jmespath.search(expression, data_list, options=jmespath_options)
 print("--*--"*10, f"Using jmespath expression: '{expression}'", dumps(search_tag_value_1234, indent=2), "", sep='\n')
